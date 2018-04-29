a15cd3ff0e264e455f328d9438905909793808ad
Nisarga Patel <np8@umbc.edu>
2018-04-29 16:58:13 -0400

    Added index.html

--- src/index.py
+++ src/index.py
@@ -3,12 +3,12 @@
 ## 
 ##
 ###################################
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def gameview():
+    return render_template('index.html', title="Home")
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=5000, debug=True)
