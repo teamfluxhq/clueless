ff87a70df6098ba6f0c363215709dd143c457e88
Triet Pham <phamtriminhtriet@gmail.com>
2018-05-06 18:09:43 -0700

    Added turns and players limit, issue update ui after pressing start game

--- src/index.py
+++ src/index.py
@@ -13,7 +13,9 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 socketio = SocketIO(app)
 
 globalGameState = {
-    "players": [] 
+    "players": [],
+    "turn": 0,
+    "current_player": ""
 }
 
 @app.route('/')
@@ -36,14 +38,25 @@ def handle_message(data):
     if given_action["action"] == "JOIN_GAME":
         playerName = given_action["payload"]["playerName"]
         if not playerName in globalGameState["players"]:
-            globalGameState["players"].append(playerName)
-            response = {
-                "responseToken": "PLAYER_JOINED_GAME",
-                "payload": playerName,
-                "gameState": globalGameState
-            }
-            responseStr = json.dumps(response)
-            emit('message', responseStr, broadcast=True)
+            # Ensure there is always 3 - 6 players
+            # Temporarily adjust to 2 for ease of testing
+            if len(globalGameState["players"]) < 6:
+                globalGameState["players"].append(playerName)
+                response = {
+                    "responseToken": "PLAYER_JOINED_GAME",
+                    "payload": playerName,
+                    "gameState": globalGameState
+                }
+                responseStr = json.dumps(response)
+                emit('message', responseStr, broadcast=True)
+            else:
+                response = {
+                    "responseToken": "MAXIMUM_PLAYER_REACHED",
+                    "payload": playerName,
+                    "gameState": globalGameState
+                }
+                responseStr = json.dumps(response)
+                send(responseStr)
         else:
             response = {
                 "responseToken": "PLAYER_ALREADY_JOINED",
@@ -63,6 +76,27 @@ def handle_message(data):
         }
         responseStr = json.dumps(response)
         emit('message', responseStr, broadcast=True)
+    elif given_action["action"] == "START_GAME":
+        print('Received startgame message')
+        globalGameState["current_player"] = globalGameState["players"][0]
+        print(globalGameState["current_player"])
+        response = {
+            "responseToken": "GAME_STARTED_STATE",
+            "payload": "Game Started",
+            "gameState": globalGameState
+        }
+        responseStr = json.dumps(response)
+        emit(responseStr)
+    elif given_action["action"] == "END_TURN":
+        globalGameState["turn"] += 1
+        globalGameState["current_player"] = globalGameState["players"][globalGameState["turn"] % 6]
+        response = {
+            "responseToken": "PLAYER_STATE",
+            "payload": "Player State",
+            "gameState": globalGameState
+        }
+        responseStr = json.dumps(response)
+        emit(responseStr)        
     else:
         response = {
             "responseToken": "UNIDENTIFIED_ACTION",
@@ -71,6 +105,15 @@ def handle_message(data):
         }
         responseStr = json.dumps(response)
         send(responseStr)
+
+# Randomly creates the answer envelop
+def createEnvelop():
+    pass
+
+# Randomly assign cards to players
+def assignCards():
+    pass
+
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True, passthrough_errors=False)
--- src/static/client.js
+++ src/static/client.js
@@ -9,6 +9,11 @@ let actions = {
     CLIENT_CONNECTION: 'CLIENT_CONNECTION',
     JOIN_GAME: 'JOIN_GAME',
     RESET_GAME: 'RESET_GAME',
+    START_GAME: 'START_GAME',
+    ACCUSE: 'ACCUSE',
+    DISPROVE: 'DISPROVE',
+    SUGGEST: 'SUGGEST',
+    END_TURN: 'END_TURN'
 }
 
 // Possible responses
@@ -16,6 +21,8 @@ let responses = {
     PLAYER_JOINED_GAME: 'PLAYER_JOINED_GAME',
     PLAYER_ALREADY_JOINED: 'PLAYER_ALREADY_JOINED',
     CLEARED_GAME_STATE: 'CLEARED_GAME_STATE',
+    MAXIMUM_PLAYER_REACHED: 'MAXIMUM_PLAYER_REACHED',
+    PLAYER_STATE: 'PLAYER_STATE'
 }
 
 // DOM elements
@@ -62,10 +69,49 @@ function resetGame() {
     socket.send(JSON.stringify(sendObjToServer))
 }
 
+function startGame() {
+    let sendObjToServer = {
+        action: actions.START_GAME,
+    }
+    socket.send(JSON.stringify(sendObjToServer))
+}
+
+function suggest() {
+    let sendObjToServer = {
+        action: actions.SUGGEST,
+    }
+    socket.send(JSON.stringify(sendObjToServer))
+}
+
+function disprove() {
+    let sendObjToServer = {
+        action: actions.DISPROVE,
+    }
+    socket.send(JSON.stringify(sendObjToServer))
+}
+
+function accuse() {
+    let sendObjToServer = {
+        action: actions.ACCUSE,
+    }
+    socket.send(JSON.stringify(sendObjToServer))
+}
+
+function endTurn() {
+    let sendObjToServer = {
+        action: actions.END_TURN,
+    }
+    socket.send(JSON.stringify(sendObjToServer))
+}
+
 
 socket.on('message', (data) => {
     let parsedMessage = JSON.parse(data);
     let gameState = parsedMessage.gameState;
+
+    statusDiv = document.getElementById("status");
+    statusDiv.innerHTML = "Current turn number: " + gameState.turn + " Current player's turn: " + gameState.current_player;
+
     switch (parsedMessage.responseToken) {
         case responses.PLAYER_JOINED_GAME:
             roomDiv = document.getElementById("room");
@@ -80,6 +126,16 @@ socket.on('message', (data) => {
                 roomDiv.innerHTML += playerNamesRendered[i];
             }
             roomDiv.innerHTML += "</ul>";
+
+            // When there are at least 3 players, the game can start
+            // hence, we enabled the start game button
+            if (gameState.players.length >= 3) {
+                startButton = document.getElementById('startGameButton');
+                if (startButton.style.display === "none") {
+                    startButton.style.display = "block";
+                }
+            }
+
             break;
         case responses.PLAYER_ALREADY_JOINED:
             playerNamePrompt = document.getElementById("playerNamePrompt");
@@ -88,8 +144,28 @@ socket.on('message', (data) => {
             playerNamePrompt.hidden = false;
             playerNamePromptErr.innerHTML = "Player with that name has already joined the game";
             break;
+        case responses.MAXIMUM_PLAYER_REACHED:
+            playerNamePrompt = document.getElementById("playerNamePrompt");
+            playerNamePromptErr = document.getElementById("playerNamePromptErr");
+
+            playerNamePrompt.hidden = false;
+            playerNamePromptErr.innerHTML = "Maximum number of players (6) for this game has been reached";
+            break;    
         case responses.CLEARED_GAME_STATE:
             location.reload();
+            break;
+        case responses.GAME_STARTED_STATE:
+            startButton = document.getElementById('startGameButton');
+            startButton.style.display = "none";
+            // document.getElementById('resetGameButton').style.display = "none";
+            if (GLOBAL_CLIENT_STATE.connectedPlayerName === gameState.current_player) {
+                document.getElementById('suggestButton').style.display = "block";
+                document.getElementById('accuseButton').style.display = "block";
+                document.getElementById('endTurnButton').style.display = "block";
+            }
+            break;
+        case responses.PLAYER_STATE:
+            break;
         default:
             console.log(parsedMessage);
     }
